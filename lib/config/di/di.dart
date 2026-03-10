5a660cbfe779112291e5a6baaeddac6fb6d72589
Youssef Singer <youssefsinger2@gmail.com>
2026-03-10 03:15:55 +0200

    feat/add reusable elevated button

--- lib/config/di/di.dart
+++ lib/config/di/di.dart
@@ -1,9 +1,9 @@
 import 'package:dio/dio.dart';
-import 'package:exam_app/config/secure_storage.dart';
+import 'package:exam_app/config/cashe/secure_storage.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
-import 'api/api_client.dart';
+import '../api/api_client.dart';
 
 var getIt = GetIt.instance;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:exam_app/config/di.dart';
+import 'package:exam_app/config/di/di.dart';
 import 'package:exam_app/core/values/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
